824dcb3b03907bb9bad722c21e554fe33ce6d41b
laxman singh <laxmansingh790616@gmail.com>
2025-06-17 20:56:25 +0530

    mega blog

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ function Home() {
       <Container>
         <div className="flex flex-wrap">
             {posts.map(post => (
-                <div key ={post.$id} className='p-2 w-1/2'>
+                <div key ={post.$id} className='p-2 sm:w-1/2 md:w-1/3  lg:w-1/4'>
                     <PostCard {...post} />
                 </div>
             ))}
